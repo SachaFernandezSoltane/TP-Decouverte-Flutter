6c0dd06fd09e9932d52867d67e002339eb9a52fa
Fernandez-Soltane Sacha <sacha.fernandez-soltane@etudiant.univ-rennes1.fr>
2025-01-30 20:31:10 +0100

    Initial commit

---
+++ lib/app.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:tp1/presentation/home.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:go_router/go_router.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final GoRouter _router = GoRouter(
+      routes: <RouteBase>[
+        GoRoute(
+          path: '/',
+          builder: (BuildContext context, GoRouterState state) {
+            return MyHomePage();
+          },
+          routes: <RouteBase>[
+        GoRoute(
+          path: 'details',
+          builder: (BuildContext context, GoRouterState state) {
+            return const MyHomePage();
+          },
+        ),
+      ],
+        ),
+      ],
+    );
+
+    return MaterialApp.router(
+      title: 'Page',
+      localizationsDelegates: AppLocalizations.localizationsDelegates,
+      supportedLocales: AppLocalizations.supportedLocales,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: const Color.fromARGB(255, 255, 8, 197),
+        ),
+        useMaterial3: true,
+      ),
+      routerConfig: _router, // Ajout de la configuration du router
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:tp1/app.dart';
+
+void main() {
+  runApp(const MyApp());
+}
